456abdef121628797dfdc7692e32cfdf13e54190
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-04-03 17:12:54 +0200

    completed

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -10,53 +10,30 @@ const albums = document.querySelector('#albums');
 const singles = document.querySelector('#singles');
 const compilations = document.querySelector('#compilations');
 
-popularBtn.addEventListener('click', () => {
-    popularBtn.classList.add('white-bg');
-    popularReleases.classList.remove('display-none');
-    albumsBtn.classList.remove('white-bg');
-    albums.classList.add('display-none');
-    singlesBtn.classList.remove('white-bg');
-    singles.classList.add('display-none');
-    compilationsBtn.classList.remove('white-bg');
-    compilations.classList.add('display-none');
-});
-
-albumsBtn.addEventListener('click', () => {
-    popularBtn.classList.remove('white-bg');
-    popularReleases.classList.add('display-none');
-    albumsBtn.classList.add('white-bg');
-    albums.classList.remove('display-none');
-    singlesBtn.classList.remove('white-bg');
-    singles.classList.add('display-none');
-    compilationsBtn.classList.remove('white-bg');
-    compilations.classList.add('display-none');
-});
-
-singlesBtn.addEventListener('click', () => {
-    popularBtn.classList.remove('white-bg');
-    popularReleases.classList.add('display-none');
-    albumsBtn.classList.remove('white-bg');
-    albums.classList.add('display-none');
-    singlesBtn.classList.add('white-bg');
-    singles.classList.remove('display-none');
-    compilationsBtn.classList.remove('white-bg');
-    compilations.classList.add('display-none');
-});
-
-compilationsBtn.addEventListener('click', () => {
+const chooseDiscography = (button, content) => {
     popularBtn.classList.remove('white-bg');
     popularReleases.classList.add('display-none');
     albumsBtn.classList.remove('white-bg');
     albums.classList.add('display-none');
     singlesBtn.classList.remove('white-bg');
     singles.classList.add('display-none');
-    compilationsBtn.classList.add('white-bg');
-    compilations.classList.remove('display-none');
-});
+    compilationsBtn.classList.remove('white-bg');
+    compilations.classList.add('display-none');
+
+    button.classList.add('white-bg');
+    content.classList.remove('display-none');
+};
+
+popularBtn.addEventListener('click', () => chooseDiscography(popularBtn, popularReleases));
+
+albumsBtn.addEventListener('click', () => chooseDiscography(albumsBtn, albums));
+
+singlesBtn.addEventListener('click', () => chooseDiscography(singlesBtn, singles));
+
+compilationsBtn.addEventListener('click', () => chooseDiscography(compilationsBtn, popularReleases));
 
 
 // fade out ONLY banner-bg on scroll (keep text)
-
 const main = document.querySelector('#main');
 const bannerBg = document.querySelector('#banner-bg');
 
